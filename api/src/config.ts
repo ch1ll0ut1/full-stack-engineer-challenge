567aa3302c598a4d46da0440fbee42411a9a8b38
Stefan Knoch <st3ve.knoch@gmail.com>
2019-11-10 18:30:11 +0700

    fix entity loading of typeorm not working

--- api/src/config.ts
+++ api/src/config.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config({ path: '.env' });
 
 export interface IConfig {
+  isDevMode: boolean;
   port: number;
   debugLogging: boolean;
   dbsslconn: boolean;
@@ -14,13 +15,14 @@ export interface IConfig {
 const isDevMode = process.env.NODE_ENV == 'development';
 
 const config: IConfig = {
+  isDevMode: isDevMode,
   port: +process.env.PORT || 3000,
   debugLogging: isDevMode,
   dbsslconn: !isDevMode,
   jwtSecret: process.env.JWT_SECRET || 'your-secret-whatever',
   databaseUrl: process.env.DATABASE_URL || 'postgres://user:pass@localhost:5432/apidb',
   dbEntitiesPath: [
-    ...isDevMode ? ['src/entity/**/*.ts'] : ['dist/entity/**/*.js'],
+    ...isDevMode ? ['src/models/**/*.ts'] : ['dist/models/**/*.js'],
   ],
 };
 
