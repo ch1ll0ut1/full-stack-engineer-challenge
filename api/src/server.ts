00bd9522d7d1c301aa379813e3a69c8e47b9e6a0
Stefan Knoch <st3ve.knoch@gmail.com>
2019-11-10 15:54:38 +0700

    disable jwt validation

--- api/src/server.ts
+++ api/src/server.ts
@@ -53,7 +53,7 @@ createConnection({
 
     // JWT middleware -> below this line routes are only reached if JWT token is valid, secret as env variable
     // do not protect swagger-json and swagger-html endpoints
-    app.use(jwt({ secret: config.jwtSecret }).unless({ path: [/^\/swagger-/] }));
+    // app.use(jwt({ secret: config.jwtSecret }).unless({ path: [/^\/swagger-/] }));
 
     // These routes are protected by the JWT middleware, also include middleware to respond with "Method Not Allowed - 405".
     app.use(protectedRouter.routes()).use(protectedRouter.allowedMethods());
