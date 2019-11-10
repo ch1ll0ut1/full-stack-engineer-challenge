1ee95a2f9105619b68d3b3cb45e11d325603f015
Stefan Knoch <st3ve.knoch@gmail.com>
2019-11-10 18:33:03 +0700

    disable synchronize for production (would cause data loss on every startup...)

--- api/src/server.ts
+++ api/src/server.ts
@@ -16,50 +16,52 @@ import { protectedRouter } from './protectedRoutes';
 // Get DB connection options from env variable
 const connectionOptions = PostgressConnectionStringParser.parse(config.databaseUrl);
 
-// create connection with database
-// note that its not active database connection
-// TypeORM creates you connection pull to uses connections from pull on your requests
-createConnection({
-    type: 'postgres',
-    host: connectionOptions.host,
-    port: connectionOptions.port,
-    username: connectionOptions.user,
-    password: connectionOptions.password,
-    database: connectionOptions.database,
-    synchronize: true,
-    logging: false,
-    entities: config.dbEntitiesPath,
-    extra: {
-        ssl: config.dbsslconn, // if not development, will use SSL
-    }
-}).then(async connection => {
+const connectDb = () => {
+    return createConnection({
+        type: 'postgres',
+        host: connectionOptions.host,
+        port: connectionOptions.port,
+        username: connectionOptions.user,
+        password: connectionOptions.password,
+        database: connectionOptions.database,
+        synchronize: config.isDevMode,
+        logging: false,
+        entities: config.dbEntitiesPath,
+        extra: {
+            ssl: config.dbsslconn, // if not development, will use SSL
+        }
+    })
+}
 
-    const app = new Koa();
+connectDb()
+    .then(async (connection) => {
+        const app = new Koa();
 
-    // Provides important security headers to make your app more secure
-    app.use(helmet());
+        // Provides important security headers to make your app more secure
+        app.use(helmet());
 
-    // Enable cors with default options
-    app.use(cors());
+        // Enable cors with default options
+        app.use(cors());
 
-    // Logger middleware -> use winston as logger (logging.ts with config)
-    app.use(logger(winston));
+        // Logger middleware -> use winston as logger (logging.ts with config)
+        app.use(logger(winston));
 
-    // Enable bodyParser with default options
-    app.use(bodyParser());
+        // Enable bodyParser with default options
+        app.use(bodyParser());
 
-    // these routes are NOT protected by the JWT middleware, also include middleware to respond with "Method Not Allowed - 405".
-    app.use(unprotectedRouter.routes()).use(unprotectedRouter.allowedMethods());
+        // these routes are NOT protected by the JWT middleware, also include middleware to respond with "Method Not Allowed - 405".
+        app.use(unprotectedRouter.routes()).use(unprotectedRouter.allowedMethods());
 
-    // JWT middleware -> below this line routes are only reached if JWT token is valid, secret as env variable
-    // do not protect swagger-json and swagger-html endpoints
-    // app.use(jwt({ secret: config.jwtSecret }).unless({ path: [/^\/swagger-/] }));
+        // JWT middleware -> below this line routes are only reached if JWT token is valid, secret as env variable
+        // do not protect swagger-json and swagger-html endpoints
+        // app.use(jwt({ secret: config.jwtSecret }).unless({ path: [/^\/swagger-/] }));
 
-    // These routes are protected by the JWT middleware, also include middleware to respond with "Method Not Allowed - 405".
-    app.use(protectedRouter.routes()).use(protectedRouter.allowedMethods());
+        // These routes are protected by the JWT middleware, also include middleware to respond with "Method Not Allowed - 405".
+        app.use(protectedRouter.routes()).use(protectedRouter.allowedMethods());
 
-    app.listen(config.port);
+        app.listen(config.port);
 
-    console.log(`Server running on port ${config.port}`);
+        console.log(`Server running on port ${config.port}`);
 
-}).catch(error => console.log('TypeORM connection error: ', error));
+    })
+    .catch(error => console.log('TypeORM connection error: ', error));
